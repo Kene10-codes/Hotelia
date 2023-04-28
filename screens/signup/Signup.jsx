083856768e57773e3a4d4ee428060de4441e9ea3
Nwobodo <nwobodokenechukwu2@gmail.com>
2023-04-28 17:24:58 +0100

    working on the signup page

--- screens/signup/Signup.jsx
+++ screens/signup/Signup.jsx
@@ -1,12 +1,58 @@
-import React from 'react';
-import {View, Text} from 'react-native';
+import {useState} from 'react';
+import {View, Text, SafeAreaView, TextInput} from 'react-native';
 
 import styles from './signup';
 
 export default function Signup () {
   return (
-    <View style={styles.container}>
-      <Text>Signup</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header__container}>
+        <Text style={styles.header}>Hotel Staff Management</Text>
+        <Text>Admin Only!</Text>
+      </View>
+      <View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Name</Text>
+          <TextInput
+            onChangeText={() => {}}
+            value=""
+            placeholder="Enter your name"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Phone Number</Text>
+          <TextInput
+            onChangeText={() => {}}
+            value=""
+            placeholder="Enter your Phone Number"
+            maxLength={11}
+            keyboardType="numeric"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Password</Text>
+          <TextInput
+            onChangeText={() => {}}
+            value=""
+            placeholder="Enter a Password"
+            style={styles.input}
+            maxLength={8}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Confirm Password</Text>
+          <TextInput
+            onChangeText={() => {}}
+            value=""
+            maxLength={8}
+            placeholder="Confirm Password"
+            style={styles.input}
+          />
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
--- screens/signup/signup.js
+++ screens/signup/signup.js
@@ -2,6 +2,31 @@ import {StyleSheet} from 'react-native';
 
 const styles = StyleSheet.create ({
   container: {flex: 1, alignItems: 'center', justifyContent: 'center'},
+  header__container: {
+    marginBottom: 50,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 700,
+    paddingBottom: 10,
+  },
+  input__wrapper: {
+    marginTop: 6,
+    marginBottom: 6,
+  },
+  input__text: {
+    fontSize: 15,
+    fontWeight: 500,
+  },
+  input: {
+    height: 42,
+    width: 300,
+    marginTop: 5,
+    marginBottom: 5,
+    borderWidth: 0.5,
+    padding: 10,
+    borderRadius: 10,
+  },
 });
 
 export default styles;
