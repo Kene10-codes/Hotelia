ea25537462832c49d450d4f25642321594c4e6fc
Nwobodo <nwobodokenechukwu2@gmail.com>
2023-05-14 16:18:59 +0100

    Made some updates

--- screens/home/Home.jsx
+++ screens/home/Home.jsx
@@ -1,12 +1,109 @@
-import React from 'react';
-import {View, Text, StyleSheet} from 'react-native';
+import {useState} from 'react';
+import RNPickerSelect from 'react-native-picker-select';
+import {
+  View,
+  Text,
+  SafeAreaView,
+  TextInput,
+  TouchableHighlight,
+  Button,
+} from 'react-native';
 
 import styles from './home';
 
-export default function Home () {
+export default function Home({navigation}) {
+  const [hotelName, setHotelName] = useState ('');
+  const [name, setName] = useState ('');
+  const [passport, setPassport] = useState ('');
+  const [position, setPosition] = useState ('');
+  const [salary, setSalary] = useState ('');
+  const [state, setState] = useState ('');
+  const [promoted, setPromoted] = useState (false);
+
   return (
-    <View style={styles.container}>
-      <Text>Staff Dashboard</Text>
-    </View>
+    <SafeAreaView style={styles.container}>
+      <View style={styles.header__container}>
+        <Text style={styles.header}>Staff Data</Text>
+      </View>
+      <View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Hotel Name</Text>
+          <TextInput
+            onChangeText={setHotelName}
+            value={hotelName}
+            placeholder="Hotel Name"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Name</Text>
+          <TextInput
+            onChangeText={setName}
+            value={name}
+            placeholder="Name"
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Passport</Text>
+          <TextInput
+            onChangeText={setPassport}
+            value={passport}
+            placeholder="Passport"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Position</Text>
+          <TextInput
+            onChangeText={setPosition}
+            value={position}
+            placeholder="Position"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Salary</Text>
+          <TextInput
+            onChangeText={setSalary}
+            value={salary}
+            placeholder="Salary"
+            style={styles.input}
+          />
+        </View>
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>State</Text>
+          <TextInput
+            onChangeText={setState}
+            value={state}
+            placeholder="State"
+            style={styles.input}
+          />
+        </View>
+
+        <View style={styles.input__wrapper}>
+          <Text style={styles.input__text}>Promoted</Text>
+          <RNPickerSelect
+            onValueChange={value => setPromoted (value)}
+            items={[
+              {label: 'Yes', value: 'true'},
+              {label: 'No', value: 'false'},
+            ]}
+          />
+        </View>
+
+        <View style={styles.submitButton}>
+          <TouchableHighlight>
+            <Button
+              style={styles.submitButton}
+              title="Add Staff"
+              onPress={() => {}}
+            />
+          </TouchableHighlight>
+        </View>
+
+      </View>
+    </SafeAreaView>
   );
 }
--- screens/home/home.js
+++ screens/home/home.js
@@ -7,6 +7,41 @@ const styles = StyleSheet.create ({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  header__container: {
+    marginBottom: 50,
+  },
+  header: {
+    fontSize: 20,
+    fontWeight: 700,
+    paddingBottom: 10,
+  },
+  input__wrapper: {
+    marginTop: 6,
+    marginBottom: 6,
+  },
+  input__text: {
+    fontSize: 15,
+    fontWeight: 400,
+  },
+  input: {
+    height: 42,
+    width: 300,
+    marginTop: 5,
+    marginBottom: 5,
+    borderWidth: 0.5,
+    padding: 10,
+    borderRadius: 5,
+  },
+  submitButton: {
+    height: 40,
+    marginTop: 15,
+  },
+  text__container: {
+    alignItems: 'center',
+    justifyContent: 'center',
+    marginTop: 2,
+    marginBottom: 2,
+  },
 });
 
 export default styles;
